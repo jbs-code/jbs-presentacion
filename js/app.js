53f2536cf9c468a64ad01598b55ed13181f98388
Jaime BS <jaimejb.code@gmail.com>
2021-01-31 19:12:42 -0600

    responsive svg

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 const pantalla = document.querySelector("#pantalla").getBBox();
 const posPantalla = document.querySelector(".pantalla");
 
-console.log(pantalla.x);
-
 function screen(){
     posPantalla.style.top = pantalla.y + "px";
     posPantalla.style.left = pantalla.x + "px";
